a20547b174553108411d912015a65cd6ee400120
Aaron Wright <aaronjohnwright@gmail.com>
2025-11-07 20:53:53 +0000

    Reformat

--- deployment.py
+++ deployment.py
@@ -3,8 +3,8 @@ from prefect import flow
 if __name__ == "__main__":
     flow.from_source(
         source="https://github.com/ABZ-Aaron/prefect-server-test.git",
-        entrypoint="code.py:my_flow",
+        entrypoint="pipelines/code.py:my_flow",
     ).deploy(
         name="my-github-deployment",
-        work_pool_name="default-agent-pool",
+        work_pool_name="my-work-pool",
     )
